d1e5fdcae5eace15f2f9c5ea086e0c4c62e0ae3b
Telkom Design Team <53335869+telkom-design@users.noreply.github.com>
2022-07-27 16:25:18 +0700

    p

--- node_modules/@legion-theme/agree/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./agree.cjs.production.min.js')
-} else {
-  module.exports = require('./agree.cjs.development.js')
-}
