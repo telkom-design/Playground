82e051c4435dfabedad658f73df0478220f33e72
Telkom Design Team <53335869+telkom-design@users.noreply.github.com>
2022-07-27 16:20:54 +0700

    Add file

---
+++ node_modules/@legion-theme/agree/dist/index.d.ts
@@ -0,0 +1,9 @@
+import deepmerge from 'deepmerge';
+export * from './colors';
+export * from './spacers';
+export * from './thypograpy';
+export * from './tokens/foundation';
+export * from './theme-agr';
+export * from './theme-agl';
+export * from './theme-aqf';
+export { deepmerge as merge };
---
+++ node_modules/@legion-theme/agree/dist/thypograpy.d.ts
@@ -0,0 +1,203 @@
+export declare const text: {
+    body: {
+        lg: {
+            reguler: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            semibold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            bold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            italic: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline_semibold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline_bold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+        };
+        sm: {
+            reguler: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            semibold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            bold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            italic: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline_semibold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline_bold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+        };
+    };
+    caption: {
+        lg: {
+            reguler: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            semibold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            bold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            italic: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline_semibold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline_bold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+        };
+        sm: {
+            reguler: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            semibold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            bold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+            };
+            italic: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline_semibold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+            underline_bold: {
+                fontSize: string;
+                lineHeight: string;
+                fontWeight: string;
+                fontStyle: string;
+            };
+        };
+    };
+    heading: {
+        h1: {
+            fontSize: string;
+            lineHeight: string;
+            fontWeight: string;
+        };
+        h2: {
+            fontSize: string;
+            lineHeight: string;
+            fontWeight: string;
+        };
+        h3: {
+            fontSize: string;
+            lineHeight: string;
+            fontWeight: string;
+        };
+        h4: {
+            fontSize: string;
+            lineHeight: string;
+            fontWeight: string;
+        };
+        h5: {
+            fontSize: string;
+            lineHeight: string;
+            fontWeight: string;
+        };
+        h6: {
+            fontSize: string;
+            lineHeight: string;
+            fontWeight: string;
+        };
+    };
+};
+export default text;
